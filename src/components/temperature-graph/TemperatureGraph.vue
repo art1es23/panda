a24eb4d46dff63ecb5385844f4c4274babc1c557
qmpwwsd <dreamwinamounts@gmail.com>
2023-03-13 00:34:36 +0200

    update: add points on graph

--- src/components/temperature-graph/TemperatureGraph.vue
+++ src/components/temperature-graph/TemperatureGraph.vue
@@ -17,6 +17,7 @@ export default {
       HEIGHT: 200,
       WIDTH: 800,
       PADDING: 40,
+      CIRCLE_RADIUS: 10,
     };
   },
 
@@ -43,10 +44,14 @@ export default {
   },
 
   methods: {
-    init(canvas, data = []) {
+    initGraph(canvas, data = []) {
+      let mouse = {
+        paint: this.paint,
+      };
       console.log("sss: ", data);
       const ctx = canvas.getContext("2d");
 
+      let reqAnimFrame;
       canvas.style.width = this.WIDTH + "px";
       canvas.style.height = this.HEIGHT + "px";
 
@@ -60,9 +65,57 @@ export default {
         TEMP_DATA.push(Math.round(obj.averageTemp));
       });
 
+      let handler = {
+        set(...args) {
+          const result = Reflect.set(...args);
+          reqAnimFrame = requestAnimationFrame(function () {
+            mouse.paint(ctx, COUNT_POINTS, data, TEMP_DATA, { mouse });
+          });
+          return result;
+        },
+      };
+      const proxy = new Proxy(mouse, handler);
+
+      // Mouse events
+      function mousemove({ clientX }) {
+        const { left } = canvas.getBoundingClientRect();
+        proxy.mouse = {
+          x: (clientX - left) * 2,
+        };
+      }
+
+      function mouseleave() {
+        proxy.mouse = null;
+      }
+
+      canvas.addEventListener("mousemove", mousemove);
+      canvas.addEventListener("mouseleave", mouseleave);
+
+      return {
+        init() {
+          mouse.paint(ctx, COUNT_POINTS, data, TEMP_DATA);
+        },
+        destroy() {
+          cancelAnimationFrame(reqAnimFrame);
+          canvas.removeEventListener("mousemove", mousemove);
+          canvas.removeEventListener("mouseleave", mouseleave);
+        },
+      };
+    },
+
+    clear(ctx) {
+      ctx.clearRect(0, 0, this.DPI_WIDTH, this.DPI_HEIGHT);
+    },
+
+    paint(ctx, COUNT_POINTS, data, TEMP_DATA, mouse) {
+      // Find min max value of temp
+      this.clear(ctx);
+      // console.log("paint");
       const [Y_min, Y_max] = this.computeBoundaries(TEMP_DATA);
       const yRatio = this.VIEW_HEIGHT / (Y_max - Y_min);
       const xRatio = this.VIEW_WIDTH / COUNT_POINTS;
+
+      // console.log(proxy.mouse);
 
       let coords = [];
       let xData = [];
@@ -83,16 +136,49 @@ export default {
         ]);
         this.paintLines(ctx, coords);
       });
-      this.xAxis(ctx, xData, xRatio);
+      this.xAxis(ctx, xData, xRatio, mouse.mouse);
+      for (const [x, y] of coords) {
+        console.log("ssss: ", this.isOver(mouse, x, coords.length));
+        if (this.isOver(mouse.mouse, x, coords.length, "#ссс")) {
+          console.log(x);
+          this.paintCircle(ctx, [x, y]);
+          break;
+        }
+      }
+
+      // console.log("mouse", mouse);
     },
 
-    xAxis(ctx, data, xRatio) {
+    isOver({ mouse }, x, length) {
+      if (!mouse) return false;
+      const width = this.DPI_WIDTH / length;
+      return Math.abs(x - mouse.x) < width / 2;
+    },
+
+    xAxis(ctx, data, xRatio, mouse) {
+      // console.log(mouse);
       ctx.beginPath();
+      ctx.fillStyle = "#ccc";
+
+      ctx.lineWidth = 1;
+      // ctx.fillStyle = "#96a2aa";
       for (let i = 0; i < data.length; i++) {
         const text = this.dateBuilder(data[i]);
         const x = i * xRatio;
-        ctx.fillText(text.toString(), x, this.DPI_HEIGHT - 10);
+        ctx.fillText(text.toString(), x, this.DPI_HEIGHT);
+
+        if (this.isOver(mouse, x, data.length)) {
+          ctx.save();
+          console.log("over");
+          ctx.moveTo(x, this.PADDING);
+
+          ctx.lineTo(x, this.DPI_HEIGHT - this.PADDING);
+
+          ctx.restore();
+        }
+        // console.log(this.isOver(mouse, x, data.length));
       }
+      ctx.stroke();
       ctx.closePath();
     },
 
@@ -103,13 +189,15 @@ export default {
       ctx.beginPath();
       ctx.strokeStyle = "#bbb";
       ctx.font = "normal 30px Helvetica, sans-serif";
+      ctx.lineWidth = 1;
       ctx.fillStyle = "#96a2aa";
       for (let i = 0; i <= count; i++) {
         const yPoint = step * i;
         // const text = Math.round(Y_max - textStep * i);
         const text = Y_max - textStep * i;
+        // const text = Y_max - textStep * i;
         // console.log(text);
-        ctx.fillText(text.toString(), 0, yPoint + this.PADDING - 10);
+        ctx.fillText(text.toFixed(1).toString(), 0, yPoint + this.PADDING - 10);
         ctx.moveTo(0, yPoint + this.PADDING);
         ctx.lineTo(this.DPI_WIDTH, yPoint + this.PADDING);
       }
@@ -117,10 +205,19 @@ export default {
       ctx.closePath();
     },
 
+    paintCircle(ctx, [x, y], color) {
+      ctx.beginPath();
+      ctx.strokeStyle = color;
+      ctx.fillStyle = "#fff";
+      ctx.arc(x, y, this.CIRCLE_RADIUS, 0, Math.PI * 2);
+      ctx.fill();
+      ctx.closePath();
+    },
+
     paintLines(ctx, coords) {
       ctx.beginPath();
       ctx.lineWidth = 5;
-      ctx.strokeStyle = "#ff0000";
+      ctx.strokeStyle = "#41A286";
 
       coords.forEach((arr) => {
         const [x, y] = arr;
@@ -167,7 +264,12 @@ export default {
 
   mounted() {
     console.log(this.localForecastData);
-    this.init(document.querySelector("#graphTemp"), this.localForecastData);
+    const graph = this.initGraph(
+      document.querySelector("#graphTemp"),
+      this.localForecastData
+    );
+
+    graph.init();
   },
 };
 </script>
@@ -182,5 +284,5 @@ export default {
 #graphTemp
     width: 100%
     height: 100%
-    border: 1px solid white
+    // border: 1px solid white
 </style>
